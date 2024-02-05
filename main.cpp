c1064d3bc6baf000fd6ed214ff03b4d97d4e999a
dashdashebrahimy <112236558+dashdashebrahimy@users.noreply.github.com>
2024-02-05 09:49:23 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -105,7 +105,7 @@ void submenu(string User)
 		    else if(b==2)
 		    {
 			
-			    A2.deposit_money(str, balance,User);	
+			    A2.deposit_money(str, balance);	
 		    }   
 			else if (b == 3)
             {
