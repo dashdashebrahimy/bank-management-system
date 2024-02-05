20ada37570f7bf9b4efda37d8d942e86191f319a
FarnooshEbh <157354653+FarnooshEbh@users.noreply.github.com>
2024-02-05 12:10:14 +0330

    Add files via upload

--- account.h
+++ account.h
@@ -189,4 +189,63 @@ public:
         return balance;
     }
 };
+class Transaction : private account{
+private:
+    time_t timestamp;
+    string amount;
+    std::string type;
+
+public:
+    Transaction(const string& amount, const string &type);
+    void recordTransaction();
+    void display() const;
+    void saveToFile(ofstream &outputFile);
+    static void viewTransactionHistory(const vector<Transaction> &transactions);
+};
+Transaction::Transaction(const string& amount, const string &type) : timestamp(time(0)), amount(amount), type(type) {}
+void Transaction::recordTransaction() {
+    if (type == "deposit") {
+        deposit_money(getAccNum(), getBalance());  
+    } else if (type == "withdraw") {
+        withdraw_money(getAccNum(), getBalance()); 
+    }
+
+    cout << "Transaction recorded." << endl;
+
+    // Save the transaction to a file
+    ofstream outputFile("transactions.txt", ios::app); // Open file in append mode
+    if (outputFile.is_open()) {
+        saveToFile(outputFile);
+        outputFile.close();
+    } else {
+        cerr << "Unable to open the file for writing." << endl;
+    }
+}
+// Add the saveToFile function in Transaction class
+void Transaction::saveToFile(ofstream &outputFile) {
+    outputFile << "Timestamp: " << ctime(&timestamp);
+    outputFile << "Amount: " << amount << "\n";
+    outputFile << "Type: " << type << "\n";
+    outputFile << "-----------------\n";
+}
+
+void Transaction::display() const {
+    cout << "Timestamp: " << ctime(&timestamp);
+    cout << "Amount: " << amount << "\n";
+    cout << "Type: " << type << "\n";
+    cout << "-----------------\n";
+}
+
+
+void Transaction::viewTransactionHistory(const vector<Transaction> &transactions) {
+    if (transactions.empty()) {
+        cout << "No transactions recorded." << endl;
+        return;
+    }
+
+    cout << "Transaction History:\n";
+    for (const auto &transaction : transactions) {
+        transaction.display();
+    }
+}
 #endif
